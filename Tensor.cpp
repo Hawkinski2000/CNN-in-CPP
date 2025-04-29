25b891d68fedcccb5935daea5ad9a5bbc2a0c9be
Kieran <hawkinski2019@gmail.com>
2025-04-28 23:50:40 -0700

    feat(tensor): allocate memory and calculate strides
    Allocate memory for tensor data and calculate strides from dimensions
    and store in the strides vector.

--- Tensor.cpp
+++ Tensor.cpp
@@ -2,16 +2,38 @@
 #include "Tensor.h"
 using namespace std;
 
+// TODO: Remove constructor, use only static functions for creating
+// tensors, e.g. tensor(), empty(), zeros(), ones(), rand(), etc.
 
 // Constructor for Tensor class
-Tensor::Tensor(initializer_list<int> dims) : dimensions(dims) {}
+Tensor::Tensor(initializer_list<size_t> dims) : dimensions(dims) {
+    // Calculate the total number of elements
+    total_elements = 1;
+    for (size_t dim : dims) {
+        total_elements *= dim;
+    }
 
+    // Allocate memory for tensor
+    data = new float[total_elements];
 
-// Function to return a tensor's shape vector
-const vector<int>& Tensor::shape() const {
-    return dimensions;
+    // Calculate strides from tensor dimensions
+    strides.resize(dimensions.size());
+    size_t stride = 1;
+    for (size_t i = dimensions.size(); i-- > 0;) {
+        strides[i] = stride;
+        stride *= dimensions[i];
+    }
 }
 
+// Destructor for Tensor class
+Tensor::~Tensor() {
+    delete[] data;
+}
+
+// Function to return a tensor's shape vector
+const vector<size_t>& Tensor::shape() const {
+    return dimensions;
+}
 
 // Function to return a tensor's shape as a string
 string Tensor::shape_str() {
@@ -26,13 +48,10 @@ string Tensor::shape_str() {
     return shape;
 }
 
-
 int main() {
-    Tensor x({2, 4, 8});
+    Tensor x{2, 4, 8, 16};
 
-    vector<int> shape = x.shape();
-
-    cout << "The first dim in the tensor x is " << shape[0] << endl;
+    cout << "The first dim in the tensor x is " << x.shape()[0] << endl;
 
     cout << "The tensor x has shape "<< x.shape_str() << endl;
 
--- Tensor.h
+++ Tensor.h
@@ -7,12 +7,21 @@ using namespace std;
 
 class Tensor {
     public:
-        Tensor(initializer_list<int> dims);
-        
-        const vector<int>& shape() const;
+        // Constructor for Tensor class
+        Tensor(initializer_list<size_t> dims);
 
+        // Destructor for Tensor class
+        ~Tensor();
+        
+        // Function to return a tensor's shape vector
+        const vector<size_t>& shape() const;
+
+        // Function to return a tensor's shape as a string
         string shape_str();
 
     private:
-        vector<int> dimensions;
+        float* data = nullptr;
+        vector<size_t> dimensions;
+        vector<size_t> strides;
+        int total_elements = 0;
 };
